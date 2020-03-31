d42853da05b26d4020a84e8a4eb6fe8caad0d1c1
DUYPHAN1803 <62800079+DUYPHAN1803@users.noreply.github.com>
2020-03-31 09:12:08 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const NavBar = () => (
+  <div className="navbar">
+    <Link to="/">Feed</Link>
+    <Link to="/profile">Profile</Link>
+  </div>
+);
+
+const Template = ({ title }) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      This is the {title} page.
+    </p>
+  </div>
+);
+
+const Feed = (props) => (
+  <Template title="Feed"/>
+);
+
+const Profile = (props) => (
+  <Template title="Profile"/>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={browserHistory}>
+        <Route path="/" component={Feed}/>
+        <Route path="/profile" component={Profile}/>
+      </Router>
+    );
+  }
+}
+
+export default App;
